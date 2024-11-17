26e5033bdfe92b76704bfe64708e7eab248f8e19
BerryLu <154629329@qq.com>
2024-11-17 12:57:41 +0800

    完成欢迎界面动画

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,16 +1,11 @@
-import s from './WelcomeLayout.module.scss';
-import cloud from '../../assets/icons/cloud.svg';
-import { RouterLink } from 'vue-router';
-import { WelcomeLayout } from './WelcomeLayout';
-export const Forth = () => (
-  <WelcomeLayout>{{
-    icon: () => <img class={s.icon} src={cloud} />,
-    title: () => <h2> 云备份 <br /> 再也不怕数据丢失</h2>,
-    buttons: () => <>
-      <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-      <RouterLink to="/start" >完成</RouterLink>
-      <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-    </>
-  }}</WelcomeLayout>
-)
+import s from './welcome.module.scss';
+import { FunctionalComponent } from 'vue';
+export const Forth: FunctionalComponent = () => {
+  return <div class={s.card}>
+    <svg>
+      <use xlinkHref='#cloud'></use>
+    </svg>
+    <h2>云备份<br /> 再也不怕数据丢失</h2>
+  </div>
+}
 Forth.displayName = 'Forth'
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,16 +1,11 @@
-import s from './WelcomeLayout.module.scss';
-import chart from '../../assets/icons/chart.svg';
-import { RouterLink } from 'vue-router';
-import { WelcomeLayout } from './WelcomeLayout';
-export const Third = () => (
-  <WelcomeLayout>{{
-    icon: () => <img class={s.icon} src={chart} />,
-    title: () => <h2>数据可视化<br />收支一目了然</h2>,
-    buttons: () => <>
-      <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-      <RouterLink to="/welcome/4" >下一页</RouterLink>
-      <RouterLink to="/start" >跳过</RouterLink>
-    </>
-  }}</WelcomeLayout>
-)
+import s from './welcome.module.scss';
+import { FunctionalComponent } from 'vue';
+export const Third: FunctionalComponent = () => {
+  return <div class={s.card}>
+    <svg>
+        <use xlinkHref='#chart'></use>
+      </svg>
+    <h2>数据可视化<br />收支一目了然</h2>
+  </div>
+}
 Third.displayName = 'Third'
