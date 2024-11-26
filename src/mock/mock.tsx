66cc7e0ebc8e8c2eec7440cea513bf7a4de29930
BerryLu <154629329@qq.com>
2024-11-26 17:11:45 +0800

    收集了四个字段 kind、tag_id、happen_at 和 amount

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -1,21 +1,25 @@
 import { faker } from '@faker-js/faker'
 import { AxiosRequestConfig } from 'axios';
+
 type Mock = (config: AxiosRequestConfig) => [number, any]
+
 faker.setLocale('zh_CN');
+
 export const mockSession: Mock = (config) => {
   return [200, {
     jwt: faker.random.word()
   }]
 }
+
+let id = 0
+const createId = () => {
+  id += 1
+  return id
+}
 export const mockTagIndex: Mock = (config) => {
   const { kind, page } = config.params
   const per_page = 25
   const count = 26
-  let id = 0
-  const createId = () => {
-    id += 1
-    return id
-  }
   const createPaper = (page = 1) => ({
     page, per_page, count
   })
@@ -27,16 +31,18 @@ export const mockTagIndex: Mock = (config) => {
       kind: config.params.kind,
       ...attrs
     }))
-    const createBody = (n = 1, attrs?: any) => ({
-      resources: createTag(n), pager: createPaper(page)
-    })
-    if (kind === 'expenses' && (!page || page === 1)) {
-      return [200, createBody(25)]
-    } else if (kind === 'expenses' && page === 2) {
-      return [200, createBody(1)]
-    } else if (kind === 'income' && (!page || page === 1)) {
-      return [200, createBody(25)]
+  const createBody = (n = 1, attrs?: any) => ({
+    resources: createTag(n), pager: createPaper(page)
+  })
+
+  if (kind === 'expenses' && (!page || page === 1)) {
+    return [200, createBody(25)]
+  } else if (kind === 'expenses' && page === 2) {
+    return [200, createBody(1)]
+  } else if (kind === 'income' && (!page || page === 1)) {
+    return [200, createBody(25)]
   } else {
     return [200, createBody(1)]
   }
+
 }
