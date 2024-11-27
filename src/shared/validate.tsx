80b3ef8365193e4e07ffdffbc7d13ad87a68a3ef
BerryLu <154629329@qq.com>
2024-11-27 12:04:08 +0800

    创建标签

--- src/shared/validate.tsx
+++ src/shared/validate.tsx
@@ -37,15 +37,17 @@ export const validate = <T extends FData>(formData: T, rules: Rules<T>) => {
   })
   return errors
 }
+
 function isEmpty(value: null | undefined | string | number | FData) {
   return value === null || value === undefined || value === ''
 }
+
 export function hasError(errors: Record<string, string[]>) {
   // return Object.values(errors)
   // .reduce((result, value) => result + value.length, 0) > 0
   let result = false
   for (let key in errors) {
-    if (errors[key].length > 0) {
+    if (errors[key]?.length > 0) {
       result = true
       break
     }
