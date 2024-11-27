b108220dce1388cd07c5ecee36cdda5fed075416
BerryLu <154629329@qq.com>
2024-11-27 11:02:06 +0800

    格式化

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -8,9 +8,9 @@ export const Tags = defineComponent({
   props: {
     kind: {
       type: String as PropType<string>,
-      required: true
+      required: true,
     },
-    selected: Number
+    selected: Number,
   },
   emits: ['update:selected'],
   setup: (props, context) => {
@@ -18,41 +18,41 @@ export const Tags = defineComponent({
       return http.get<Resources<Tag>>('/tags', {
         kind: props.kind,
         page: page + 1,
-        _mock: 'tagIndex'
-      })
-    })
+        _mock: 'tagIndex',
+      });
+    });
     const onSelect = (tag: Tag) => {
-      context.emit('update:selected', tag.id)
-    }
-    return () => <>
-      <div class={s.tags_wrapper}>
-        <div class={s.tag}>
-          <div class={s.sign}>
-            <Icon name="add" class={s.createTag} />
-          </div>
-          <div class={s.name}>
-            新增
-          </div>
-        </div>
-        {tags.value.map(tag =>
-          <div class={[s.tag, props.selected === tag.id ? s.selected : '']}
-            onClick={() => onSelect(tag)}
-          >
+      context.emit('update:selected', tag.id);
+    };
+    return () => (
+      <>
+        <div class={s.tags_wrapper}>
+          <div class={s.tag}>
             <div class={s.sign}>
-              {tag.sign}
+              <Icon name="add" class={s.createTag} />
             </div>
-            <div class={s.name}>
-              {tag.name}
-            </div>
+            <div class={s.name}>新增</div>
           </div>
-        )}
-      </div>
-      <div class={s.more}>
-        {hasMore.value ?
-          <Button class={s.loadMore} onClick={fetchTags}>加载更多</Button> :
-          <span class={s.noMore}>没有更多</span>
-        }
-      </div>
-    </>
-  }
-})
+          {tags.value.map((tag) => (
+            <div
+              class={[s.tag, props.selected === tag.id ? s.selected : '']}
+              onClick={() => onSelect(tag)}
+            >
+              <div class={s.sign}>{tag.sign}</div>
+              <div class={s.name}>{tag.name}</div>
+            </div>
+          ))}
+        </div>
+        <div class={s.more}>
+          {hasMore.value ? (
+            <Button class={s.loadMore} onClick={fetchTags}>
+              加载更多
+            </Button>
+          ) : (
+            <span class={s.noMore}>没有更多</span>
+          )}
+        </div>
+      </>
+    );
+  },
+});
